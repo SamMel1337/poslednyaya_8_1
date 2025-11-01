b562a245290e008b8ce707d3e8523a47c10d646b
SOMEWELL <melkuman147@gmail.com>
2025-11-01 20:39:40 +0400

    добавления в файл2

--- habit/views.py
+++ habit/views.py
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.core.paginator import Paginator
-from models import Habit, HabitTracking
+from .models import Habit, HabitTracking
 from serializers import (
     UserRegistrationSerializer,
     UserSerializer,
