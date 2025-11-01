9b9d9899a60368031ac0e09eae76751ac98baccd
SOMEWELL <melkuman147@gmail.com>
2025-11-01 21:07:57 +0400

    добавления в файл 122

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
