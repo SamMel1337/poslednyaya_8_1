d9033f9b33ce4d2147b3a53f4e79858191f74fa7
SOMEWELL <melkuman147@gmail.com>
2025-11-01 20:50:26 +0400

    добавления в файл2

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from habit.models import HabitTracking
-from serializers import (
+from .serializers import (
     UserRegistrationSerializer,
     UserLoginSerializer,
     UserProfileSerializer,
