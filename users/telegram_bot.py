57c3c061d3ae053493075d4c8af16db2d7cfc410
SOMEWELL <melkuman147@gmail.com>
2025-11-01 20:58:40 +0400

    добавления в файл2

--- users/telegram_bot.py
+++ users/telegram_bot.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.conf import settings
-from models import User
+from .models import User
 from telegram_service import TelegramService
 
 
